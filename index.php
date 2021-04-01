13fbcaae9fc99b1ffa0300dc4b79b88dfff0feb2
edwardtimoty <edwardtimoty@gmail.com>
2021-04-01 23:35:29 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Git remote 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quaerat eum a provident natus, quos consectetur delectus inventore tempora! Aliquid natus placeat esse dolorem nisi, vero et earum quaerat minima.</p>
 </body>
 </html>   
